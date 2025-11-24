f6ad3072fc7367f3287a85a34a4ef5c900c4058c
tpa1kalyan <tpa1kalyan@gmail.com>
2025-11-25 01:31:56 +0530

    commit

--- Login.py
+++ Login.py
@@ -9,16 +9,12 @@ class Login:
 
   def test(self):
     driver = self.driver
-    # Changed find_elements to find_element
     driver.find_element(By.ID,"student-tab").click()
     time.sleep(4)
-    # Changed find_elements to find_element
     driver.find_element(By.NAME,"admission_no").send_keys("0788/IS/2022-23")
     time.sleep(2)
-    # Changed find_elements to find_element
     driver.find_element(By.CLASS_NAME,"student_pass").send_keys("pavan@123")
     time.sleep(2)
-    # Changed find_elements to find_element
     driver.find_element(By.ID,"login-student").click()
     time.sleep(1000)
 
@@ -28,5 +24,4 @@ if __name__ == "__main__":
   auto = Login()
   auto.setup()
   auto.test()
-  # Corrected typo: suto.teardown() to auto.teardown()
   auto.teardown()
