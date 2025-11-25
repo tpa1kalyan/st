02c566de4bc231e6038228a373996b670be91c2a
pavankalyan-24 <oavan.is22@sahyadri.edu.in>
2025-11-25 10:06:54 +0530

    st lab

---
+++ Google.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+class Google:
+    def setup(self):
+        self.driver =webdriver.Chrome()
+        
+    def search_loacction(self):
+        g_link=("https://www.google.com/maps")
+        ip_link='//*[@id="searchboxinput"]'
+        direction_xpath= '//*[@id="searchbox-searchbutton"]'
+        self.driver.get(g_link)
+        time.sleep(2)
+        name =str(input("Enter location name: "))
+        self.driver.find_element(By.XPATH,ip_link).send_keys(name)
+        self.driver.find_element(By.XPATH,direction_xpath).click()
+        time.sleep(5)
+    
+    def teardown(self):
+        self.driver.quit()
+if __name__ == "__main__":
+    g=Google()
+    g.setup()
+    g.search_loacction()
+    g.teardown()
